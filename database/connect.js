8fa93f7201915e12fa9c397244b5caca75b1d181
Bassem1234 <bassem.rbaia1@gmail.com>
2022-06-22 16:02:01 +0200

    db

--- database/connect.js
+++ database/connect.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-DBURL = 'mongodb+srv://bassem:24111991aA@cluster0.ec7n2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true
 };
 
 mongoose
-  .connect(DBURL, options)
+  .connect(process.env.DBURL, options)
   .then(() => console.log("Connected to Mongo"))
   .catch((err) => console.log(err));
